d600ce65d33f2f621d1dc96a510fc116085b4515
Hugo Madureira <hugoomdra@gmail.com>
2020-04-29 08:48:30 +0200

    feat(bluetooth): ajout fichier bluetooth

---
+++ appli/bluetooth.c
@@ -0,0 +1,13 @@
+/*
+ * bluetooth.c
+ *
+ *  Created on: 28 avr. 2020
+ *      Author: hmadureira
+ */
+
+#include "bluetooth.h"
+
+void sendData(int16_t data[]){
+
+	//TODO, envoie des données
+}
---
+++ appli/bluetooth.h
@@ -0,0 +1,13 @@
+/*
+ * bluetooth.h
+ *
+ *  Created on: 28 avr. 2020
+ *      Author: hmadureira
+ */
+
+#ifndef BLUETOOTH_H_
+#define BLUETOOTH_H_
+
+void sendData(int16_t data[]);
+
+#endif /* BLUETOOTH_H_ */
